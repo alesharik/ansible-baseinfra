2ba7f58cd49e18019dca3a97c1fb2d6d149318d0
alesharik <alesharik4@gmail.com>
2026-07-04 09:26:32 +0300

    fix(clickhouse): users not working

--- roles/clickhouse/templates/init-defaults.sh
+++ roles/clickhouse/templates/init-defaults.sh
@@ -19,28 +19,3 @@ echo "Prometheus config written"
 echo "Creating database {{ db }}"
 clickhouse-client -u "${CLICKHOUSE_USER}" --password "${CLICKHOUSE_PASSWORD}" --query "CREATE DATABASE IF NOT EXISTS {{ db }}";
 {% endfor %}
-
-echo "Writing users config"
-cat <<EOT > /etc/clickhouse-server/users.d/users.xml
-<!-- Docs: <https://clickhouse.tech/docs/en/operations/settings/settings_users/> -->
-<clickhouse>
-<users>
-{% for u in (clickhouse.users | dict2items) %}
-  <{{ u.key }}>
-    <profile>default</profile>
-    <networks>
-      <ip>::/0</ip>
-    </networks>
-    <password>{% raw %}${{% endraw %}{{ u.key | upper }}{% raw %}_PASSWORD}{% endraw %}</password>
-    <quota>default</quota>
-    <grants>
-{% for g in (u.value.grants or []) %}
-      <query>{{ g }}</query>
-{% endfor %}
-    </grants>
-  </{{ u.key }}>
-{% endfor %}
-</users>
-</clickhouse>
-EOT
-#cat /etc/clickhouse-server/users.d/user.xml;
